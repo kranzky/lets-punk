203e72314be4414b434b3160811c3a1a8a56d478
Jason Hutchens <jasonhutchens@gmail.com>
2021-01-09 14:15:38 +0800

    implement basic specs with postgresql

--- env/test.sh
+++ env/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export DATABASE_URL='postgres://localhost/punk_test'
+export DATABASE_URL='postgres://localhost/lets_punk_test'
 export MEMCACHIER_SERVERS='localhost:11211'
 export COOKIE_SECRET='1d6f64082fe77be66525cfb9cc8ed089ada6cd366ef62b3aae1eb94c7acd8389faca60239159c54f214ba7c2e36f6ea47dff421069205828c60b4769d789e278'
